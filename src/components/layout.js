50aa75bba72733b148403c4bb7c8d97e35a84a86
harim choi <harim1206@gmail.com>
2020-02-28 13:02:56 -0500

    simplify

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,23 +6,9 @@
  */
 
 import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
-
-import Header from "./header"
 import "./layout.css"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
     <>
       <div>
@@ -30,10 +16,6 @@ const Layout = ({ children }) => {
       </div>
     </>
   )
-}
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
 }
 
 export default Layout
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,13 +6,30 @@ import main from "../styles/main.scss"
 
 const IndexPage = () => (
   <Layout>
-    <div class="hello">what'sup</div>
-    <div class="">what'sup</div>
+    <div className="container">
+      <div className="header">Harim Choi</div>
+
+      <div className="main">
+        <a href="www.bollandbranch.com"><div className="project">Boll & Branch</div></a>
+        <a href="www.10pastten.com"><div className="project">10 Past Ten</div></a>
+        <a href="www.baboontothemoon.com"><div className="project">Baboon To The Moon</div></a>
+        <a href="www.onthedash.com"><div className="project">On The Dash</div></a>
+        <a href="thirtythreefourtyfive.herokuapp.com"><div className="project">33/45</div></a>
+        <a href="www.nightsandweekends.info"><div className="project">Nights & Weekends</div></a>
+      </div>
     
-    
-    
+    </div>    
     
   </Layout>
 )
 
 export default IndexPage
+/*
+
+Boll and Branch
+10PastTen
+OnTheDash
+BaboonToTheMoon
+33/45
+Nights and Weekends
+*/
