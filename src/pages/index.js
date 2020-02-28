7cd23e5ebdf298f52213c2a59ecbaae705f68e0c
harim choi <harim1206@gmail.com>
2020-02-28 10:36:00 -0500

    commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,17 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import main from "../styles/main.scss"
+
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi peoples</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <div class="hello">what'sup</div>
+    <div class="">what'sup</div>
+    
+    
+    
+    
   </Layout>
 )
 
