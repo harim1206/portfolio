6c774190012a211ae66d98d319b3e5dfec82de32
harim choi <harim1206@gmail.com>
2020-03-03 15:09:04 -0500

    config fix

---
+++ src/components/images.js
@@ -0,0 +1,20 @@
+import bbImg from '../images/bollbranch.png'
+import ttImg from '../images/tenpastten.png'
+import bttmImg from '../images/baboontothemoon.png'
+import otdImg from '../images/onthedash.png'
+import ttffImg from '../images/thirtythreefourtyfive.png'
+import nwGif from '../images/nw.gif'
+import harimImg from '../images/harim.png'
+
+
+
+
+export {
+  bbImg,
+  ttImg,
+  bttmImg,
+  otdImg,
+  ttffImg,
+  nwGif,
+  harimImg
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,58 +6,61 @@ import Name from "../components/name"
 import Preview from "../components/preview"
 import "../styles/main.scss"
 
-// import data from '../assets/data'
-import {
-  bbImg,
-  ttImg,
-  bttmImg,
-  otdImg,
-  ttffImg,
-  nwGif,
-  harimImg
-} from '../assets/images'
+import data from '../components/data'
+// import {
+//   bbImg,
+//   ttImg,
+//   bttmImg,
+//   otdImg,
+//   ttffImg,
+//   nwGif,
+//   harimImg
+// } from '../components/images'
 
 
 class IndexPage extends Component {
   
   state = {
-    data: {
-      bollandbranch: {
-        title: "Boll & Branch",
-        href: "https://www.bollandbranch.com",
-        img: bbImg,
-      },
-      tenpastten: {
-        title: "10 Past Ten",
-        href: "http://www.10pastten.com",
-        img: ttImg,
-      },
-      baboontothemoon:  {
-        title: "Baboon to the Moon",
-        href: "https://www.baboontothemoon.com",
-        img: bttmImg,
-      },
-      onthedash: {
-        title: "On The Dash",
-        href: "http://www.onthedash.com",
-        img: otdImg,
-      },
-      thirtythree: {
-        title: "33/45",
-        href: "https://thirtythreefourtyfive.herokuapp.com",
-        img: ttffImg,
-      },
-      nightsandweekends: {
-        title: "Nights & Weekends",
-        href: "https://www.nightsandweekends.info",
-        img: nwGif,
-      },
-      resume: {
-        title: "Resume",
-        href: "https://drive.google.com/file/d/1SdRH1QKfvYaV9ruFuq_8xV931XNrW5zr/view",
-        img: harimImg,
-      }
-    }
+    data
+
+    // data: {
+    //   bollandbranch: {
+    //     title: "Boll & Branch",
+    //     href: "https://www.bollandbranch.com",
+    //     img: bbImg,
+    //   },
+    //   tenpastten: {
+    //     title: "10 Past Ten",
+    //     href: "http://www.10pastten.com",
+    //     img: ttImg,
+    //   },
+    //   baboontothemoon:  {
+    //     title: "Baboon to the Moon",
+    //     href: "https://www.baboontothemoon.com",
+    //     img: bttmImg,
+    //   },
+    //   onthedash: {
+    //     title: "On The Dash",
+    //     href: "http://www.onthedash.com",
+    //     img: otdImg,
+    //   },
+    //   thirtythree: {
+    //     title: "33/45",
+    //     href: "https://thirtythreefourtyfive.herokuapp.com",
+    //     img: ttffImg,
+    //   },
+    //   nightsandweekends: {
+    //     title: "Nights & Weekends",
+    //     href: "https://www.nightsandweekends.info",
+    //     img: nwGif,
+    //   },
+    //   resume: {
+    //     title: "Resume",
+    //     href: "https://drive.google.com/file/d/1SdRH1QKfvYaV9ruFuq_8xV931XNrW5zr/view",
+    //     img: harimImg,
+    //   }
+    // }
+
   }
 
   onHover = e => {
